bde34698b4576c27f8c1752cb0b4006bf59ec20e
ghost <106078876+mercedesov@users.noreply.github.com>
2024-06-07 16:09:38 +0400

    add files

---
+++ daynineteen1.py
@@ -0,0 +1,28 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def show_area(self):
+        return self.length * self.width
+
+    def show_perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def scale(self, factor):
+        self.length *= factor
+        self.width *= factor
+
+rectangle = Rectangle(4, 3)
+print("Length:", rectangle.length)
+print("Width:", rectangle.width)
+print("Area:", rectangle.show_area())
+print("Perimeter:", rectangle.show_perimeter())
+
+rectangle.scale(2)
+print("Length:", rectangle.length)
+print("Width:", rectangle.width)
+print("Area:", rectangle.show_area())
+print("Perimeter:", rectangle.show_perimeter())
+
+
